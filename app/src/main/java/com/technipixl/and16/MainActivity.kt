3db349166377423eec11f5230143021e9e305a8b
Mavrik <r.mavrik@gmail.com>
2022-11-22 15:20:37 +0100

    + comments

--- app/src/main/java/com/technipixl/and16/MainActivity.kt
+++ app/src/main/java/com/technipixl/and16/MainActivity.kt
@@ -30,40 +30,69 @@ class MainActivity : AppCompatActivity() {
 		)
 
 		binding.menuButton.setOnClickListener {
-			when (isMenuActivated) {
-				true -> {
-					val animSet = AnimatorSet()
-					animSet.playTogether(menuAnims)
-					animSet.reverse()
-				}
-				false -> {
-					val animSet = AnimatorSet()
-					animSet.playTogether(menuAnims)
-					animSet.start()
-				}
-			}
-			isMenuActivated = !isMenuActivated
+			toggleMenuWithAnimations(menuAnims)
 		}
 	}
 
+	/**
+	 * Description: function that open or close a menu
+	 *
+	 * @param animations List of all the animations to play when opening or closing the menu
+	 */
+	private fun toggleMenuWithAnimations(animations: List<ValueAnimator>) {
+		when (isMenuActivated) {
+			true -> {
+				val animSet = AnimatorSet()
+				animSet.playTogether(animations)
+				animSet.reverse()
+			}
+			false -> {
+				val animSet = AnimatorSet()
+				animSet.playTogether(animations)
+				animSet.start()
+			}
+		}
+		isMenuActivated = !isMenuActivated
+	}
+
+	/**
+	 * Description: function that take all menu items and return all animations to make a left
+	 * vertical menu.
+	 *
+	 * @param items the views to animate from top to bottom
+	 *
+	 * @return [List] of [ValueAnimator]
+	 */
 	private fun getMenuAnims(vararg items: View) : List<ValueAnimator> {
-		val endValueInterval = (180 / (items.size + 1)).toFloat()
+		val endValueInterval = (180f / (items.size + 1))
 		val finalDelay = (items.size - 1) * 150
 		val finalList: MutableList<ValueAnimator> = mutableListOf()
 
 		items.forEachIndexed { index, view ->
 			val endValue = (index + 1) * endValueInterval
-			val delay = (finalDelay - (index * 150)).toLong()
+			val delay = (finalDelay - (index * 150L))
 			finalList.addAll(getMenuItemAnims(view, endValue, delay))
 		}
 
 		return finalList
 	}
 
+	/**
+	 * Description: function that take a view, generate rotation and alpha animations and return them
+	 * in a list
+	 *
+	 * @param view the view to animate
+	 *
+	 * @param endValue the end position angle (in degrees)
+	 *
+	 * @param delay the delay before starting the animation (default value is 0)
+	 *
+	 * @return [List] of 2 [ValueAnimator]
+	 */
 	private fun getMenuItemAnims(view: View, endValue: Float, delay: Long = 0) : List<ValueAnimator> {
 
 		val angleAnim = ValueAnimator.ofFloat(0f, endValue).apply {
-			duration = 1000 - delay/2
+			duration = 1_000 - delay/2
 			startDelay = delay
 
 			addUpdateListener {
